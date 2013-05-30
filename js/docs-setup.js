d6fcb5e3eb754253c4cb503b008656f7462bdb32
Pascal Precht <pascal.precht@googlemail.com>
2013-05-30 19:23:41 +0200

    update site

---
+++ js/docs-setup.js
@@ -0,0 +1,104 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Reference",
+    "guide": "Guide"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "index",
+      "shortName": "API Reference",
+      "type": "overview",
+      "keywords": "api foo overview reference"
+    },
+    {
+      "section": "api",
+      "id": "translate.$translate",
+      "shortName": "$translate",
+      "type": "object",
+      "keywords": "$interpolate $log $q $rootscope $scope $translate actual also an and angular-translate api app asynchronously at available by care change choosed configured contents core dynamic excepts for function given hash headline_text id in interpolate interpolateparams is isn it key language let load loaders method object of optional parameters preferred preferredlanguage registered remember represents returns runtime service storage storagekey store storing takes tells the this to token translate translatedtext translation translationid tries trying use used uses values when which with your"
+    },
+    {
+      "section": "api",
+      "id": "translate.$translateProvider",
+      "shortName": "$translateProvider",
+      "type": "object",
+      "keywords": "$injector $tranlateprovider $translatecookiestorage $translatelocalstorage $translateprovider $translatestaticfilesloader $translateurlloader above actual actually added allows an and angular-translate another any anywhere api app are argument arguments as asynchronous at be behavior build built-in by can choosed concrete config configuration configure custom customhandler de_de define defined dep1 dep2 developers directly does doesn don either en_us error example exist expects extended extension factory first for function general get gets given greeting guys hallo handler hash have headline headline_text hello here hey id in initial inside instantiated invoking is isn it javascript job just key langkey language later layer ll loaded loader loaderfactory loaders log method missingtranslationhandler module multiple must name new no object of old once one only optional options or other parameter pass passing plain preferredlanguage prefix provided register registered registering registers related represent represents return returns same says service set sets shortcut similar since some_text something specific startup storage storagekey storageprefix table tables tell tells text that the there this throw to too translate translation translation-tables translationid translations translationtable trying two url use usecookiestorage used useloader uselocalstorage usemissingtranslationhandler usemissingtranslationhandlerlog user uses usestaticfilesloader usestorage useurlloader value was welt when where which with world you your"
+    },
+    {
+      "section": "api",
+      "id": "translate.directive:translate",
+      "shortName": "translate",
+      "type": "directive",
+      "keywords": "$compile $digest $filter $interpolate $rootscope $scope $translateprovider an angular api as attribute be can config content controller could directive dom dynamic either element expect filter following function given hello html id index inject internally interpolated into is it js literal module name ng-controller ngview object optional or pascalprecht pass passed possible scenario script should string text the there through to tobe translate translatectrl translates translation translation_id translationid translations uses value values which with_values"
+    },
+    {
+      "section": "api",
+      "id": "translate.filter:translate",
+      "shortName": "translate",
+      "type": "filter",
+      "keywords": "$parse $scope $translate $translateprovider accepts angular api be config contents controller dynamic dynamized filter following function hash hello html id index interpolate interpolateparams into is js literal module name ng-controller ngview object optional or parameters pascalprecht pass script service string text the there though to translate translatectrl translated translation translation_id translationid translations uses value values with_values"
+    },
+    {
+      "section": "guide",
+      "id": "index",
+      "shortName": "Guide",
+      "type": "overview",
+      "keywords": "$translateprovider also and angular angular-translate api app as better bower checkout com config cool dependency discussions doc else features foo for full function google guide hello how if in inject install into is it its learn like ll make making md module myapp on or org out overview paragraph pascalprecht possible projects public questions quick read reference start teach the there things this title to translate translations use var via welcome what when you your"
+    },
+    {
+      "section": "guide",
+      "id": "00_installation",
+      "shortName": "Installation",
+      "type": "overview",
+      "keywords": "after ahead all and angular-translate are as bower broken build building can clone cloned code com comes components configured consider contains course created dependencies development do down during easily ever everything execute featured files fix folder following for framework from fuck full generate get git github go green grunt guide have here html if in install installation installing is it jasmine js just karma like look machine make matter min much navigate now of on otherwise overview package passed please pretty production project properly provided provides pull raw ready repository request right run running see send should simply since so source start successfully sure tasks testing testrunner tests testsuite thanks that the them these things this to tools unit use used uses using version very via waits want watch well what where which whole will with work you your"
+    },
+    {
+      "section": "guide",
+      "id": "01_getting-started",
+      "shortName": "Getting Started",
+      "type": "overview",
+      "keywords": "$scope $translate $translateprovider able above access actually again all also an and angular anywhere app approach apps are as at attribute be because beeing behaves behavior better bind but by can cases certain check co com comes complex component1 component2 components concrete configuration configure configuring content contents controller controllers could ctrl decouples dependency different directive directly do don easy etc example expect expressions filter filters follows from function get getting gives great guide guys ha handling hard hash having headline headline_text here hey how i18n id if in inject into is it js just key know l310-l326 l40-l60 language languages layer logic make many method module module-wide more most much myapp namespaces namespacing object of one organization others out overview pascalprecht pass passing phase place plunker possible pretty provided provides re register registered represents service services sets several show shows simply since some_text specific started support table tables teach teaching tell tests text that the there this time to too translate translated translatedtext translation translations turned two up use used useful using value values var version view want watch way ways what when where which why with would wouldn you your"
+    },
+    {
+      "section": "guide",
+      "id": "02_dynamize_your_translations",
+      "shortName": "Dynamize your translations",
+      "type": "overview",
+      "keywords": "$scope $translate $translateprovider _n_ able above achieve actually afterwards all also an and angular another_text any are argument as attribute available be because beers before bind but by can cases check co com combine controller could course covered ctrl currently data directive directives display do done dynamic dynamize expects exposes expression fairly far filter filters first fix following for function get gets getting good got guessed guide had hard has hash have identifier if in information internally interpolated interpolation into is isn it js just key kind l373-l448 l714-l830 l86-l142 like literal mails make maps means message might module more myapp named neat need not notification now number object of okay on only or out overview parameters parametrizing pascalprecht pass passed passing passing-values-through-translate-service place placeholder plunker possible pretty provided received registered required result right same say scope second service simple since so somehting special specific string syntax teach test testsuite text that the then there this through to translate translatedtext translation translationdata translations use uses using value values ve view want way ways we well what where which with works would you your"
+    },
+    {
+      "section": "guide",
+      "id": "03_multi-language",
+      "shortName": "Multi Language",
+      "type": "overview",
+      "keywords": "$http $scope $translate $translatecookiestorage $translatelocalstorage $translateprovider about actually all also an and angular angular-translate angular-translate-storage-cookie angular-translate-storage-local app are as at available be both bower build but by calls can care choosed co comes config controller cookie cookiestorage could create cross ctrl current custom de_de dep2 dep3 do don en_en english event example except extension extensions factory fallback find first fit following for function german get greeting guide hallo has have hello here http if in install instead internally is it key know language languages let like ll local localstorage look make maybe means method module more multi multiple myapp mycustomservice needs no now on once one our over overview own parameter pasalprecht pascalprecht pass passed plunker possible preferredlanguage provided provides really register registered remember requests return runtime same saying scope second service set shortcut simply since so some something storage storages store sure switch table tables teach teaching tells that the then there this to togglelanguage told translate translation translations usage use usecookiestorage used uselocalstorage uses usestorage value ve via view want we welt where which with world you your"
+    },
+    {
+      "section": "guide",
+      "id": "04_asynchronous-loading",
+      "shortName": "Asynchronous Loading",
+      "type": "overview",
+      "keywords": "$http $q $translate $translateprovider $translateurlloader able above actually addition adds again all already also an and angular angular-translate angular-translate-loader-static-files angular-translate-loader-url another any app architecture are as asynchronous asynchronously at autoload automatically available be behave better bower but by can case charm clever co combine config contains control cool course custom customloader data de_de declare defer deferred describes different do doesn either en_us endpoint even example existing exists expects extension factory file files first fit following foo fooooo for free from full function gets given go guide happens has have haven hello hello_text here how if in information install invoke invoked is isn it json just key lang language languages later lazy least let like ll load loaded loader loaderfn loaders loading locale- locale-de_de locale-en_us localization make match module mymodule needed needs new non notices now object of okay on once one or other others overview parameter pattern plunker possibilities possible possibly preferredlanguage prefix pretty probably promise properties property provides re real register registering reject rejected request requested requests required resolve resolved return returns right runtime say says server service several should signatures simplest simply since so some somebody something soon specific specifies specify startup static-files string suffix support sure table tell telling text that the there they this three to transform translation translations two until url use useloader uses usestaticfilesloader useurlloader using valid var via view want way ways what when where which will with without works world yet you your"
+    },
+    {
+      "section": "guide",
+      "id": "05_error-handling",
+      "shortName": "Error Handling",
+      "type": "overview",
+      "keywords": "$translateprovider able all an and angular angular-translate angular-translate-handler-log another app be besides bower by call called console course custom define defining dep1 dep2 do does doesn error exist expects extend extension factory for function functionality gets guide handler handling has have how id if install into is like log logs look method missing module myapp mycustomhandlerfactory name of on overview own pascalprecht re return returns so something tell that the there this to tranlation translate translation translations try use usemissingtranslationhandler usemissingtranslationhandlerlog using var via want warnings which with you your yourself"
+    },
+    {
+      "section": "guide",
+      "id": "06_migration-guide",
+      "shortName": "Migration Guide",
+      "type": "overview",
+      "keywords": "$translateprovider also angular-translate are async care changed com cookie done extensions for from get guide https information instead interfaces introducing is let-your-app-remember-the-language ll loader loading local longer make migration module name new ngtranslate no now of overview pascalprecht please preferredlanguage registering-asynchronous-loaders registerloader rememberlanguage repos rest see some static-files storage storages supported sure take the there they to translate url use usecookiestorage uselocalstorage uses usestorage using work"
+    }
+  ],
+  "scripts": [
+    "angular.min.js",
+    "angular-translate.min.js"
+  ],
+  "html5Mode": false
+};
