9744f75b4510bff417be56ac24cef7d2f5f83b78
Pascal Precht <pascal.precht@googlemail.com>
2013-07-23 19:17:00 +0200

    Automatic gh-pages commit

--- plato/files/filter_translate_js/report.history.js
+++ plato/files/filter_translate_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 23 Jul 2013 17:14:42 GMT","sloc":60,"lloc":5,"functions":2,"deliveredBugs":0.06243204266092216,"maintainability":82.48026112309044,"lintErrors":0,"difficulty":7.142857142857143}]
+__history = [{"date":"Tue, 23 Jul 2013 17:16:59 GMT","sloc":60,"lloc":5,"functions":2,"deliveredBugs":0.06243204266092216,"maintainability":82.48026112309044,"lintErrors":0,"difficulty":7.142857142857143}]
