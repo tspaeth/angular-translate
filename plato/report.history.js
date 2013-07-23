9c7959790b2b78b0a36e382fe4a76e4375bfc8e9
Pascal Precht <pascal.precht@googlemail.com>
2013-07-23 19:14:43 +0200

    Automatic gh-pages commit

--- plato/report.history.js
+++ plato/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 23 Jul 2013 16:56:30 GMT","total":{"sloc":180,"maintainability":159.95865249366963},"average":{"sloc":90,"maintainability":"79.98"}}]
+__history = [{"date":"Tue, 23 Jul 2013 17:14:42 GMT","total":{"sloc":180,"maintainability":159.95865249366963},"average":{"sloc":90,"maintainability":"79.98"}}]
